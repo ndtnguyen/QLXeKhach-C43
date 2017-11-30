afa94951d76684490a5485d0fda281513c3edf5c
Nguyễn Đào Thảo Nguyên <ndtnguyen9596@gmail.com>
2017-11-30 12:49:12 +0700

    initialize source

---
+++ 03_Source/C43QLXeKhach/C43QLXeKhach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="C43QLXeKhach.MvcApplication" Language="C#" %>
---
+++ 03_Source/C43QLXeKhach/C43QLXeKhach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(C43QLXeKhach.Startup))]
+namespace C43QLXeKhach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
