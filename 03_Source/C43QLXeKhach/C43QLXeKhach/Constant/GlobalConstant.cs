b4bfcde4cdbe05753539232193bf41c82ac963c4
ndtnguyen <ndtnguyen9596@gmail.com>
2017-12-07 01:55:41 +0700

    upd

---
+++ 03_Source/C43QLXeKhach/C43QLXeKhach/Constant/GlobalConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace C43QLXeKhach.Constant
+{
+    static class GlobalConstant
+    {
+        public const string USER = "C43QLXEKHACH_USER";
+        public const string SESSION_ID = "C43QLXEKHACH_SESSIONID";
+    }
+}
