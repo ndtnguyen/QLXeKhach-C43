0927250cbc40090b85c65b91686356bce6cd1768
1412053 <1412053@student.hcmus.edu.vn>
2017-12-11 22:35:33 +0700

    1412053 - Tạo class

---
+++ 03_Source/C43QLXeKhach/C43QLXeKhach/Services/TRAMXEsService/ITramXeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using C43QLXeKhach.Models;
+namespace C43QLXeKhach.Services.TRAMXEsService
+{
+    public interface ITramXeService
+    {
+        //IList<TINHTHANH> GetAll();
+        //int Add(TINHTHANH tt);
+        //void Update(TINHTHANH tt);
+        //void Delete(TINHTHANH tt);
+        //TINHTHANH Detail(string id);
+        //void Dispose();
+        //IList<TINHTHANH> Search(string input);
+
+    }
+}
---
+++ 03_Source/C43QLXeKhach/C43QLXeKhach/Services/TRAMXEsService/TramXeService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using C43QLXeKhach.Models;
+using System.Data.Entity;
+using System.Text;
+using C43QLXeKhach.Constant;
+using C43QLXeKhach.Utils;
+
+namespace C43QLXeKhach.Services.TRAMXEsService
+{
+    public class TramXeService : ITramXeService
+    {
+        //public IList<TINHTHANH> GetAll()
+        //{
+        //    using (QLXeKhachEntities context = new QLXeKhachEntities())
+        //    {
+        //        return context.TINHTHANHs.Where(x => x.isDeleted != 1).ToList();
+        //    }
+        //}
+
+        //public int Add(TINHTHANH tt)
+        //{
+        //    using (QLXeKhachEntities context = new QLXeKhachEntities())
+        //    {
+        //        var user = HttpContext.Current.Session[GlobalConstant.USER];
+        //        if (user != null)
+        //        {
+        //            NHANVIEN currentUser = (NHANVIEN)user;
+        //            tt.createUser = currentUser.MaNV;
+        //            tt.lastupdateUser = currentUser.MaNV;
+        //        }
+        //        DateTime current = DateTime.Now;
+        //        tt.createDate = current;
+        //        tt.lastupdateDate = current;
+        //        context.TINHTHANHs.Add(tt);
+        //        context.SaveChanges();
+        //        return 1;
+        //    }
+        //}
+
+        //public void Update(TINHTHANH tt)
+        //{
+        //    var user = HttpContext.Current.Session[GlobalConstant.USER];
+        //    if (user != null)
+        //    {
+        //        NHANVIEN currentUser = (NHANVIEN)user;
+        //        tt.lastupdateUser = currentUser.MaNV;
+        //    }
+        //    DateTime current = DateTime.Now;
+        //    tt.lastupdateDate = current;
+        //    using (QLXeKhachEntities context = new QLXeKhachEntities())
+        //    {
+        //        context.Entry(tt).State = EntityState.Modified;
+        //        context.SaveChanges();
+        //    }
+        //}
+
+        //public void Delete(TINHTHANH tt)
+        //{
+        //    var user = HttpContext.Current.Session[GlobalConstant.USER];
+        //    if (user != null)
+        //    {
+        //        NHANVIEN currentUser = (NHANVIEN)user;
+        //        tt.lastupdateUser = currentUser.MaNV;
+        //    }
+        //    DateTime current = DateTime.Now;
+        //    tt.lastupdateDate = current;
+        //    using (QLXeKhachEntities context = new QLXeKhachEntities())
+        //    {
+        //        context.Entry(tt).State = EntityState.Modified;
+        //        context.SaveChanges();
+        //    }
+        //}
+
+        //public TINHTHANH Detail(string id)
+        //{
+        //    using (QLXeKhachEntities context = new QLXeKhachEntities())
+        //    {
+        //        return context.TINHTHANHs.Find(id);
+        //    }
+        //}
+
+        //public void Dispose()
+        //{
+        //    using (QLXeKhachEntities context = new QLXeKhachEntities())
+        //    {
+        //        context.Dispose();
+        //    }
+        //}
+
+
+        //public IList<TINHTHANH> Search(string input)
+        //{
+        //    using (QLXeKhachEntities context = new QLXeKhachEntities())
+        //    {
+        //        return context.TINHTHANHs.Where(x => x.isDeleted != 1 && (x.TenTT.Contains(input) || input == "")).ToList();
+        //    }
+        //}
+
+
+
+    }
+}
