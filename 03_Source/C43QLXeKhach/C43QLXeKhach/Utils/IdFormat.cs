3cd54cc8175f9d8b97a11ff836b135bee0b3fcd9
ndtnguyen <ndtnguyen9596@gmail.com>
2017-12-21 23:03:14 +0700

    add format utils

---
+++ 03_Source/C43QLXeKhach/C43QLXeKhach/Utils/IdFormat.cs
@@ -0,0 +1,27 @@
+﻿using C43QLXeKhach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace C43QLXeKhach.Utils
+{
+    public class IdFormat
+    {
+        public static string TinhThanhFormat(string MaTT)
+        {
+            using (QLXeKhachEntities context = new QLXeKhachEntities())
+            {
+                TINHTHANH tt = context.TINHTHANHs.Find(MaTT);
+                if (tt != null)
+                {
+                    return tt.TenTT;
+                } else
+                {
+                    return "N/A";
+                }
+                
+            }
+        }
+    }
+}
