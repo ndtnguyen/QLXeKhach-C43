378a0ce3740624d89b6a29289b378c76110b043c
Nguyễn Đào Thảo Nguyên <ndtnguyen9596@gmail.com>
2017-12-23 00:15:17 +0700

    commit missing files

---
+++ 03_Source/C43QLXeKhach/C43QLXeKhach/Models/GiaCoBanEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace C43QLXeKhach.Models
+{
+    public class GiaCoBanEntity
+    {
+        public string MaTT1 { get; set; }
+        public string MaTT2 { get; set; }
+        public string MaLoai { get; set; }
+        public Nullable<decimal> GiaCoBan1 { get; set; }
+        public Nullable<int> createUser { get; set; }
+        public Nullable<int> lastupdateUser { get; set; }
+        public Nullable<System.DateTime> createDate { get; set; }
+        public Nullable<System.DateTime> lastupdateDate { get; set; }
+        public Nullable<int> isDeleted { get; set; }
+    }
+}
