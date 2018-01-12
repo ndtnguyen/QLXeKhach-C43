278fc7f2964a71dba42ad4578fe9410707211e0e
TruongLuan1996 <huuluantruong@gmail.com>
2018-01-12 16:23:56 +0700

    chỉnh sửa chuyến xe

--- 03_Source/C43QLXeKhach/C43QLXeKhach/Utils/ChuyenXeInfo.cs
+++ 03_Source/C43QLXeKhach/C43QLXeKhach/Utils/ChuyenXeInfo.cs
@@ -32,6 +32,23 @@ namespace C43QLXeKhach.Utils
                 return "";
             }
         }
+        public static string GetTuyen(int? matuyen)
+        {
+            using (QLXeKhachEntities context = new QLXeKhachEntities())
+            {
+                TUYENXE tx = context.TUYENXEs.Find(matuyen);
+                if (tx != null)
+                {
+                    TINHTHANH tt1 = context.TINHTHANHs.Find(tx.DiemDi);
+                    TINHTHANH tt2 = context.TINHTHANHs.Find(tx.DiemDen);
+                    if(tt1!=null && tt2 != null)
+                    {
+                        return tt1.TenTT + " - " + tt2.TenTT;
+                    }
+                }
+                return "";
+            }
+        }
         public static IList<XE> GetBienSo()
         {
             using (QLXeKhachEntities context = new QLXeKhachEntities())
@@ -46,6 +63,34 @@ namespace C43QLXeKhach.Utils
             {
                 return context.TAIXEs.Where(x => x.isDeleted != 1).ToList();
             }
+        }
+        public static IList<TuyenInfo> GetTuyen()
+        {
+            using (QLXeKhachEntities context = new QLXeKhachEntities())
+            {
+                IList<TuyenInfo> result = new List<TuyenInfo>();
+                var kq = (from tx in context.TUYENXEs
+                            join tt1 in context.TINHTHANHs
+                            on tx.DiemDi equals tt1.MaTT
+                            join tt2 in context.TINHTHANHs
+                            on tx.DiemDen equals tt2.MaTT
+                            select new { MaTuyen = tx.MaTuyen, DiemDi = tt1.TenTT, DiemDen = tt2.TenTT }
+                            ).ToList();
+                foreach(var item in kq)
+                {
+                    var temp = new TuyenInfo();
+                    temp.MaTuyen = item.MaTuyen;
+                    temp.DiemDi = item.DiemDi;
+                    temp.DiemDen = item.DiemDen;
+                    result.Add(temp);
+                }
+                return result;
+            }
+        }
+        public class TuyenInfo { 
+            public int MaTuyen { get; set; }
+            public string DiemDi { get; set;}
+            public string DiemDen { get; set; }
         }
     }
 }
